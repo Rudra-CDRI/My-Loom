8cc3d63f1f0cdfed53c4b3733f31d62b0c94af1f
Rudra-CDRI <rudra.cdri@gmail.com>
2026-06-07 14:34:53 +0530

    giving install option on android

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'my-loom-cache-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Simple fetch handler to satisfy PWA requirements
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
